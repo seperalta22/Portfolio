ec50e3f27bf76a573d8a3087c632c7bce7e8a182
Sergio Peralta <seperalta88@gmail.com>
2023-04-12 14:10:45 -0300

    Add email validator functionality

---
+++ js/form.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('.contact__form');
+const emailInput = document.getElementById('email');
+const emailError = document.getElementById('email-error');
+
+form.addEventListener('submit', (event) => {
+	if (emailInput.value !== emailInput.value.toLowerCase()) {
+		emailError.innerHTML = 'The email must be written in lower case.';
+		emailError.style.display = 'block';
+
+		// Prevents the form from submitting
+		event.preventDefault();
+
+		// Hides the error message after 5 seconds
+		setTimeout(() => {
+			emailError.style.display = 'none';
+		}, 5000);
+	}
+});
