2dc95548334d5f84c2ef1f2751508d13f00203f1
Sergio Peralta <seperalta88@gmail.com>
2023-04-12 14:18:13 -0300

    Fix linter issues

--- js/form.js
+++ js/form.js
@@ -3,16 +3,16 @@ const emailInput = document.getElementById('email');
 const emailError = document.getElementById('email-error');
 
 form.addEventListener('submit', (event) => {
-	if (emailInput.value !== emailInput.value.toLowerCase()) {
-		emailError.innerHTML = 'The email must be written in lower case.';
-		emailError.style.display = 'block';
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    emailError.innerHTML = 'The email must be written in lower case.';
+    emailError.style.display = 'block';
 
-		// Prevents the form from submitting
-		event.preventDefault();
+    // Prevents the form from submitting
+    event.preventDefault();
 
-		// Hides the error message after 5 seconds
-		setTimeout(() => {
-			emailError.style.display = 'none';
-		}, 5000);
-	}
+    // Hides the error message after 5 seconds
+    setTimeout(() => {
+      emailError.style.display = 'none';
+    }, 5000);
+  }
 });
